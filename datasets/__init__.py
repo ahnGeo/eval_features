3a69987d3cf606546482e4840044c5c18c3c27c7
Geo Ahn <67467737+ahnGeo@users.noreply.github.com>
2023-02-27 00:28:51 +0900

    Delete datasets directory

--- datasets/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-
-from .kinetics import Kinetics  # noqa
-from .ucf101 import UCF101
-from .hmdb51 import HMDB51
-from .kth import KTH
-from .diving48 import Diving48
-# from .ssv2 import Ssv2  # noqa
